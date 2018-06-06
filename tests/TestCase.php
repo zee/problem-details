aac00ade42d7ed9fabb2195073bf89e59e7a3805
Veaceslav Medvedev <slavcopost@gmail.com>
2018-06-06 20:07:23 +0200

    Add phpunit bootstrap

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Zee Project.
+ *
+ * @see https://github.com/zee/
+ */
+
+declare(strict_types=1);
+
+namespace Zee\Problems\Tests;
+
+use PHPUnit\Framework;
+
+/**
+ * Class TestCase.
+ */
+abstract class TestCase extends Framework\TestCase
+{
+}
